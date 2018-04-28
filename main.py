43adb3dbe1c50a5dbb410e8671a28895df083849
An Tran <atran0715@gmail.com>
2018-04-27 17:29:47 -0700

    working on plotting data from each columns from data

--- main.py
+++ main.py
@@ -11,21 +11,43 @@ from pylab import scatter, show, legend, xlabel, ylabel
 
 import seaborn as sns
 from seaborn import palettes
+from tensorflow.contrib.graph_editor.select import compute_boundary_ts
 sns.set(style="white")
 sns.set(style="whitegrid", color_codes=True)
 
 
 # load data and divide it into two subsets, used for training and testing
 # category, time between deadline and launch, goal, amount pledged, number of backers
-# category: 3, goal: 6, pledged: 8, state: 9, backers: 10, time between deadline and launch: 15
+# category: 3, goal: 6, pledged: 8, backers: 10, time between deadline and launch: 15
 kickstartData = download_data('Choppeddatasetv4.csv', [3, 6, 8, 10, 15]).values 
-kickstartLabels = download_data('labels.csv',[0]) 
+kickstartLabels = download_data('Choppeddatasetv4.csv',[9]).values
+labels, kickstartLabels = np.unique(kickstartLabels[:,0], return_inverse=True)
+columns = ['category','goal','pledged','backers','days_between']
+# list of labels
+print("The labels are",labels[0],": 0 and", labels[1], ": 1.")
+
+df = pd.DataFrame(
+    data=kickstartData,
+    columns=columns)
+
+#kickstartLabels = download_data('labels.csv',[0])
+
+
+### display Success and failures by category ###
+pd.crosstab(kickstartData[:,0],kickstartLabels).plot(kind='bar')
+plt.title('Success and Failures by Category')
+plt.xlabel('Job')
+plt.ylabel('Frequency of Genres')
+plt.savefig('Genres')
+plt.show()
 
 # replace category string values to floats
-#[ 'Art', 'Comics', 'Crafts', 'Dance', 'Design', 'Fashion', 'Film & Video', 'Food',
-# 'Games', 'Journalism', 'Music', 'Photography', 'Publishing', 'Technology', 'Theater']
 category, category_num = np.unique(kickstartData[:,0], return_inverse=True)
 kickstartData[:,0] = category_num
+
+# list of categories
+print("List of categories:",category)
+
 
 print("Data Shape" , kickstartData.shape)
 print("Label Data Shape", kickstartLabels.shape)
@@ -33,59 +55,78 @@ print("Label Data Shape", kickstartLabels.shape)
 # split the data into testing and training with a 20/80 split
 x_train, x_test, y_train, y_test = train_test_split(kickstartData, kickstartLabels, test_size=0.20)
 
+df = pd.DataFrame(
+    data=x_test,
+    columns=columns)
+goal_max = df['goal'].max()
+goal_min = df['goal'].min()
+
+pt = x_test[:,1].transpose()
+g_min = x_test[:,1].min()
+g_max = x_test[:,1].max()
+
+
+filter_val = np.array([20000000.0,40000000.0,60000000.0,80000000.0,100000000.0])
+filter_val = filter_val.astype(float)
+np.dtype()
+categ = np.digitize(pt,filter_val,right=True)
+
+print(g_min, g_max)
+filter_values = [g_min+5000,g_min+10000,g_max]
+p_u, pt = np.unique(pt, return_inverse = True)
+print(p_u)
+p = pd.cut(pt, 5).value_counts()
+print(p)
+# print(p)
+# pd_group_cut = df.groupby(pd.cut(x_test[:,0], 15)).sum()
+# print(pd_group_cut)
+
+# df['goal'].hist(normed=1, bins=20, stacked=False, alpha=.5)
+# plt.hist(df['goal'], bins=100, range=(goal_min,goal_max))
+# print(goal.hist(data, column=None, by=None, grid=True, xlabelsize=None,
+#                xrot=None, ylabelsize=None, yrot=None, ax=None, sharex=False,
+#                sharey=False, figsize=None, layout=None, bins=10, **kwds))
+# plt.show()
+
 # sklearn Logistic Regression
-logisticRegr = LogisticRegression()
+# logisticRegr = LogisticRegression()
 
 # reshape y_train to (39999,)
-y_train = np.array(y_train)
-y_train = y_train.reshape(39999,)
+# y_train = np.array(y_train)
+# y_train = y_train.reshape(39999,)
 # train class instance
-logisticRegr.fit(x_train, y_train)
+# logisticRegr.fit(x_train, y_train)
 
 # predictions
-predictions = logisticRegr.predict(x_test)
-print(predictions)
+# predictions = logisticRegr.predict(x_test)
+# print(predictions)
 # accuracy
-accuracy = logisticRegr.score(x_test, y_test)
+# accuracy = logisticRegr.score(x_test, y_test)
 
 
 # visualize data using functions in the library pylab
-Y = np.array(kickstartLabels)
-Y = Y.astype(float)
-Y = Y.reshape(49999,)
-print(Y.shape)
-
-X = np.array(kickstartData)
-min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-1,1))
-X = min_max_scaler.fit_transform(X)
-
-# visualize data using functions in the library pylab 
-pos = where(Y == 1)
-neg = where(Y == 0)
-scatter(X[pos, 0], X[pos, 1], marker='o', c='b')
-scatter(X[neg, 0], X[neg, 1], marker='x', c='r')
-xlabel('Feature 1: score 1')
-ylabel('Feature 2: score 2')
-legend(['Label:  Admitted', 'Label: Not Admitted'])
-show()
-
-# number of fails and success
-sns.countplot(x=Y, palette='hls')
-xlabel('Failed 0.0 : Successful 1.0')
-plt.savefig('Failed_Successful_Plot')
-
-
-pd.crosstab(kickstartData[:,0],kickstartLabels).plot(kind='bar')
-plt.title('Purchase Frequency for Job Title')
-plt.xlabel('Job')
-plt.ylabel('Frequency of Purchase')
-plt.savefig('purchase_fre_job')
-
-plt.show()
-
-
-CM, acc, arrR, arrP = func_confusion_matrix(y_test.values[:,0], predictions)
-print("Confusion matrix",CM)
-print("Accuracy: ", acc)
-print("Per-class precision rate: ",arrP)
-print("Per-class recall rate: ",arrR)
+# Y = np.array(kickstartLabels)
+# Y = Y.astype(float)
+# Y = Y.reshape(49999,)
+# print(Y.shape)
+# 
+# X = np.array(kickstartData)
+# min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-1,1))
+# X = min_max_scaler.fit_transform(X)
+# 
+# # number of fails and success
+# sns.countplot(x=Y, palette='hls')
+# xlabel('Failed 0.0 : Successful 1.0')
+# plt.savefig('Failed_Successful_Plot')
+# 
+# plt.plot(y_test,predictions, '.')
+# x = np.linspace(0,330,100)
+# y = x
+# plt.plot(x,y)
+# plt.show()
+# 
+# CM, acc, arrR, arrP = func_confusion_matrix(y_test, predictions)
+# print("Confusion matrix",CM)
+# print("Accuracy: ", acc)
+# print("Per-class precision rate: ",arrP)
+# print("Per-class recall rate: ",arrR)
